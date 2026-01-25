8f8cce25b30a6f891a132d643c37c3f9d32e5f3b
Christine Duarte <christine_dos_santos@hotmail.fr>
2026-01-25 13:54:06 +0100

    fix(adapter): feat-bank-account - Fix AccountDtoMapper unmapped overdraftLimit warning

--- src/main/java/com/bank/adapter/in/rest/mapper/AccountDtoMapper.java
+++ src/main/java/com/bank/adapter/in/rest/mapper/AccountDtoMapper.java
@@ -1,10 +1,10 @@
 package com.bank.adapter.in.rest.mapper;
 
 import com.bank.adapter.in.rest.dto.AccountDto;
-import com.bank.domain.model.Account;
 import com.bank.domain.model.CurrentAccount;
 import com.bank.domain.model.SavingsAccount;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * MapStruct mapper to convert Account domain objects to DTOs sent to the frontend.
@@ -13,12 +13,23 @@ import org.mapstruct.Mapper;
 public interface AccountDtoMapper {
 
     /**
-     * Converts an Account domain object to an AccountDto.
+     * Converts a CurrentAccount domain object to an AccountDto.
+     * SavingsAccount doesn't have overdraftLimit, so depositLimit is ignored.
      *
-     * @param account the domain object
+     * @param account the CurrentAccount domain object
      * @return the DTO for the frontend
      */
+    @Mapping(target = "depositLimit", ignore = true)
     AccountDto toCurrentAccountDto(CurrentAccount account);
+
+    /**
+     * Converts a SavingsAccount domain object to an AccountDto.
+     * CurrentAccount doesn't have depositLimit, so overdraftLimit is ignored.
+     *
+     * @param account the SavingsAccount domain object
+     * @return the DTO for the frontend
+     */
+    @Mapping(target = "overdraftLimit", ignore = true)
     AccountDto toSavingsAccountDto(SavingsAccount account);
 
 }
